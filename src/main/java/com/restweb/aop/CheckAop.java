d68a18e12c7778426d5315d5db22e7858cd43c05
Yashpal <yashpalsingh3645@gmail.com>
2021-08-19 17:09:51 +0530

    second commit

---
+++ src/main/java/com/restweb/aop/CheckAop.java
@@ -0,0 +1,9 @@
+package com.restweb.aop;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+public class CheckAop {
+ 
+	//this is just for checking purpos
+}
